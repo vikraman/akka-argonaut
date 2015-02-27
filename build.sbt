f9b19d54f3469c77c481c64fd7b3f83d2faf8519
Vikraman Choudhury <git@vikraman.org>
2015-02-27 19:01:20 +0530

    Initial version

---
+++ build.sbt
@@ -0,0 +1,28 @@
+name := "akka-argonaut"
+
+scalaVersion in ThisBuild := "2.11.5"
+
+scalacOptions in ThisBuild := Seq(
+  "-encoding", "UTF-8",
+  "-deprecation",
+  "-unchecked",
+  "-feature",
+  "-Xlint",
+  "-optimise"
+)
+
+libraryDependencies ++= {
+  val akkaV       = "2.3.9"
+  val akkaStreamV = "1.0-M3"
+  val argonautV   = "6.1-M5"
+  val scalaTestV  = "2.2.1"
+  Seq(
+    "com.typesafe.akka" %% "akka-actor"                        % akkaV,
+    "com.typesafe.akka" %% "akka-stream-experimental"          % akkaStreamV,
+    "com.typesafe.akka" %% "akka-http-core-experimental"       % akkaStreamV,
+    "com.typesafe.akka" %% "akka-http-experimental"            % akkaStreamV,
+    "com.typesafe.akka" %% "akka-http-testkit-experimental"    % akkaStreamV,
+    "io.argonaut"       %% "argonaut"                          % argonautV,
+    "org.scalatest"     %% "scalatest"                         % scalaTestV % "test"
+  )
+}
---
+++ src/main/scala/akka/http/marshallers/argonaut/ArgonautSupport.scala
@@ -0,0 +1,49 @@
+package akka.http.marshallers.argonaut
+
+import scalaz._
+import argonaut._, Argonaut._
+import akka.http.model.{ ContentTypes, HttpCharsets }
+import akka.http.model.MediaTypes.`application/json`
+import akka.http.marshalling.{ ToEntityMarshaller, Marshaller }
+import akka.http.unmarshalling.{ FromEntityUnmarshaller, Unmarshaller }
+import akka.stream.FlowMaterializer
+
+import scala.concurrent.ExecutionContext
+import scala.language.implicitConversions
+
+trait ArgonautSupport {
+
+  implicit def argonautUnmarshallerConverter[T](e: DecodeJson[T])(implicit ec: ExecutionContext, mat: FlowMaterializer): FromEntityUnmarshaller[T] =
+    argonautUnmarshaller(e, ec, mat)
+
+  implicit def argonautUnmarshaller[T](implicit e: DecodeJson[T], ec: ExecutionContext, mat: FlowMaterializer): FromEntityUnmarshaller[T] =
+    argonautJsonUnmarshaller.map(e.decodeJson(_).toDisjunction)
+
+  implicit def argonautJsonUnmarshaller(implicit ec: ExecutionContext, mat: FlowMaterializer): FromEntityUnmarshaller[Json] =
+    Unmarshaller.byteStringUnmarshaller.forContentTypes(`application/json`).mapWithCharset { (data, charset) ⇒
+      val input =
+        if (charset == HttpCharsets.`UTF-8`) data.utf8String
+        else data.decodeString(charset.nioCharset.name)
+      Parse.parse(input)
+    }
+
+  implicit def argonautMarshallerConverter[T](e: EncodeJson[T])(implicit ec: ExecutionContext, p: Pretty = prettifier): ToEntityMarshaller[T] =
+    argonautMarshaller[T](e, ec, p)
+
+  implicit def argonautMarshaller[T](implicit e: EncodeJson[T], ec: ExecutionContext, p: Pretty = prettifier): ToEntityMarshaller[T] =
+    argonautJsonMarshaller[T].compose(e.encode)
+
+  implicit def argonautJsonMarshaller[T](implicit e: EncodeJson[T], ec: ExecutionContext, p: Pretty = prettifier): ToEntityMarshaller[Json] =
+    Marshaller.StringMarshaller.wrap(ContentTypes.`application/json`)(p)
+
+  private implicit def disjunctionToException[E, T](d: \/[E, T]): T = d match {
+    case -\/(e) => throw new IllegalArgumentException("Invalid value")
+    case \/-(t) => t
+  }
+
+  type Pretty = Json => String
+  protected val prettifier: Pretty = _.nospaces
+
+}
+
+object ArgonautSupport extends ArgonautSupport
